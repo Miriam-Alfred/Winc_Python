8047053d533280e4b052e5ba5e7eb16de3cfac74
Miriam-Alfred <133356385+Miriam-Alfred@users.noreply.github.com>
2023-06-25 14:37:43 +0200

    Add files via upload
    Assignment: Arguments

--- main.py
+++ main.py
@@ -1,58 +1,40 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
 
 # Add your code after this line
 
-#players who scored
-score_1='Ruud Gullit '
-score_2='Marco van Basten '
+# Part 1: Greet Template
+def greet(name, greeting_template='Hello, <name>!'):
+    return greeting_template.replace('<name>', name)
 
-#Minute of the match that a goal was scored
-goal_0=32
-goal_1=54
+# Part 2: Force
+gravity_factors = {
+    'earth': 9.8,
+    'mercury': 3.7,
+    'venus': 8.9,
+    'mars': 3.7,
+    'jupiter': 23.1,
+    'saturn': 9.0,
+    'uranus': 8.7,
+    'neptune': 11.0,
+    'pluto': 0.7,
+    'moon': 1.6
+}
 
-#part 1.3
-scorer1=score_1 + str(goal_0)
-scorer2=score_2 + str(goal_1)
-scorers= scorer1 + ', '+scorer2
-print(scorers)
+def force(mass, body='earth'):
+    gravity = gravity_factors.get(body.lower(), 9.8)
+    return mass * gravity
 
-#part 1.4
-report_a=(score_1 + 'scored in the ' + str(goal_0) + 'nd '+ 'minute')
-report_b=(score_2 + 'scored in the ' + str(goal_1) + 'th '+ 'minute')
-report= (report_a + '\n'+ report_b)
-print(report)
+# Part 3: Gravity
+G = 6.674e-11
 
-#part 2.1 
-player='Erwin koeman'
-#first_name='Erwin'
-#last_name='Koeman' 
+def pull(m1, m2, d):
+    return G * ((m1 * m2) / (d ** 2))
 
-#part 2.2
-is_space = player.find(' ')
-first_name = player[:is_space]
-last_name = player[is_space+1:]
-print(first_name)
-
-#part 2.3
-last_name_len=len(last_name)
-print(last_name_len)
-
-#part 2.4
-Player_names=player.split()
-First_letter=Player_names[0][0]
-name_short= First_letter + '. '+ Player_names[1]
-print(name_short)
-
-#part 2.5
-times= len(first_name)
-name= f'{first_name}!' 
-chant= (name + " ") * (len(first_name) -1) + name
-print(chant)
-
-#part 2.6
-chant_check = chant[-1]
-good_chant = chant_check != ' '
-print(good_chant)
+# example_cars
+m1 = 800
+m2 = 1500
+d = 3
+print(round(pull(m1, m2, d), 10))
 
